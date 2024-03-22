03cf5fc2dc37c9b9592a9ea4e554401374a9e9fc
Quincy Michels <quincy.michels@gmail.com>
2024-03-22 14:53:58 -0400

    added graphic slider ui element, and custom game instance

---
+++ Source/ManasoupGJ3/ManasoupGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ManasoupGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANASOUPGJ3_API UManasoupGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
