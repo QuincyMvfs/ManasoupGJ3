b67869ed33a907a1285916ef8f90a738030ca2c8
Quincy Michels <quincy.michels@gmail.com>
2024-03-23 14:03:30 -0400

    added various sounds

--- Source/ManasoupGJ3/Actors/SoundManagerActor.h
+++ Source/ManasoupGJ3/Actors/SoundManagerActor.h
@@ -19,7 +19,53 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Music")
+	TArray<USoundWave*> M_GameplayMusic;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Music")
+	TArray<USoundWave*> M_MainMenuMusic;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Music")
+	TArray<USoundWave*> M_PauseMenuMusic;
+	
+	// MOVEMENT
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_AliveFootsteps;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_GhostFootsteps;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_AliveJump;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_GhostJump;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_AliveLand;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_GhostLand;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Movement")
+	TArray<USoundWave*> M_WallJump;
+
+	// DEATH
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Death")
+	TArray<USoundWave*> M_AliveDead;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Death")
+	TArray<USoundWave*> M_GhostDead;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|Death")
+	TArray<USoundWave*> M_Resurrection;
+
+	// ETC
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|UI")
+	TArray<USoundWave*> M_ButtonSelected;
+
+	// LEVEL COMPONENTS
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Sounds|LevelComponents")
+	TArray<USoundWave*> M_Wind;
 };
